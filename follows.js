89cd62d2f5c1ce30dc67a150834f22da2851e0ca
pbustos <pbustos@unex.es>
2013-12-07 10:51:19 +0100

    Update follows.js

--- follows.js
+++ follows.js
@@ -149,8 +149,10 @@
 										width: 600,
 										height: 200,
 										renderer: 'line',
-										min: parseFloat(datastream.min_value) - .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
-										max: parseFloat(datastream.max_value) + .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
+										//min: parseFloat(datastream.min_value) - .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
+										//max: parseFloat(datastream.max_value) + .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
+										min: -10,
+										max: 100,
 										padding: {
 											top: 0.02,
 											right: 0.02,
